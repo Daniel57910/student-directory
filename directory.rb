d5bbc2610b905e64f5bd8ad109d3f360fa95f259
Daniel5791 <danielmiller5791@gmail.com>
2018-03-14 09:19:34 +0000

    code refactored

--- directory.rb
+++ directory.rb
@@ -1,19 +1,18 @@
+students = [
+    "Dr. Hannibal Lecter",
+    "Darth Vader",
+    "Nurse Ratched",
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Krueger",
+    "The Joker",
+    "Joffrey Baratheon",
+    "Norman Bates"
+]
 
 puts "The students of Villians Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#finally, we print the total number of students
-print "Overall, we have "
-#it's important that print() doesn't add new line characters
-print 11
-puts " great students"
+puts "-----------------"
+students.map{ |student| puts student }
+puts "Overall, we have #{students.count} great students"
