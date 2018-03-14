af150d5faaed9d032a82a32617252a7e9a711414
Daniel5791 <danielmiller5791@gmail.com>
2018-03-14 09:46:40 +0000

    directory updated with start dates

--- directory.rb
+++ directory.rb
@@ -4,27 +4,28 @@ def print_header
 end
 
 def print_students(students)
-  students.map { |student| puts student }
+  students.map { |student, month| puts "#{student} (#{month} cohort)" }
+  #each enumerable represents row in array
 end
 
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
+  #count enumerable unlike length, eg count array for names beginning in "T"
 end
 
 students = [
-    "Dr. Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratched",
-    "Michael Corleone",
-    "Alex DeLarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates"
+  ["Dr. Hannibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Corleone", :november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator", :november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
 ]
-
 
 print_header
 print_students(students)
