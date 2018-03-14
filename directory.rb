f86eb137f430750e17530c6c5964b53d537abdfc
Daniel5791 <danielmiller5791@gmail.com>
2018-03-14 14:07:41 +0000

    additional info added + centered

--- directory.rb
+++ directory.rb
@@ -4,13 +4,18 @@ def print_header
   puts "-----------------"
 end
 
+def print_wrap(current, i)
+  info = (i + 1).to_s.concat(". #{current[:name]} is #{current[:age]} years old and #{current[:height]} cm tall. #{current[:name]}'s hobbies are #{current[:hobbs]}. They are part of the #{current[:cohort]} cohort.")
+
+  info.center(info.length + 5)
+  #center > than length of string or string returned
+end
+
 def print_students(students)
   i = 0
   while i < students.length
     current = students[i]
-    if current[:name].length < 12 and current[:name].chr == "D"
-      puts "#{i + 1}. #{current[:name]} (#{current[:cohort]} cohort)"
-    end 
+    puts print_wrap(current, i)
     i +=1
   end
 end
@@ -23,13 +28,25 @@ end
 def input_students
 
   students = []
-  puts "Please enter the names of the students"
+  puts "Please enter the following student information"
   puts "To finish, just hit return twice"
 
-  name = gets.chomp
+  name = "ENTER"
 
   until name.empty? 
-    students << {name: name, cohort: :november}
+    puts "Enter the name"
+    name = gets.chomp
+    puts "Enter the cohort"
+    cohort = gets.chomp
+    puts "Enter your height"
+    height = gets.chomp
+    puts "Enter your age"
+    age = gets.chomp
+    puts "Enter your hobbies"
+    hobbs = gets.chomp
+
+
+    students << {name: name, cohort: cohort, height: height, age: age, hobbs: hobbs}
     #name: name of hash key, :november symbol/static that immutable
     puts "Now we have #{students.count} students"
     name = gets.chomp
